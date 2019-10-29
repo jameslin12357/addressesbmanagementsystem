87b99ae3d6a1302007187dab25a4d4188c2b3898
jameslin12357 <jameslin12357@qq.com>
2019-10-29 16:07:32 +0800

    cm1

---
+++ src/main/java/com/example/addressesbmanagementsystem/AddressesbmanagementsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.addressesbmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressesbmanagementsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressesbmanagementsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/addressesbmanagementsystem/AddressesbmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.addressesbmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressesbmanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
